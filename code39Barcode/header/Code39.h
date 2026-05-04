b3e7ac89f57d39d73b362d4ca1389e1ca2d43805
sheltdoumear <voposternak.07@gmail.com>
2026-05-04 21:03:35 +0300

    map fix

--- code39Barcode/header/Code39.h
+++ code39Barcode/header/Code39.h
@@ -6,7 +6,7 @@ using namespace std;
 
 class Code39 {
 public:
-    vector<int> encode(const string& input); // вектор - последовательность полос, 1 черная, 0 белая
+        vector<int> encode(const string& input, int narrow, int wide); // вектор - последовательность полос, 1 черная, 0 белая
 };
 
 
--- code39Barcode/implementation/BarcodeImage.cpp
+++ code39Barcode/implementation/BarcodeImage.cpp
@@ -5,36 +5,35 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-// ================= PNG =================
 
 void BarcodeImage::savePNG(const std::string& filename,
                            const std::vector<int>& barcode,
                            int height,
-                           int margin)
+                           int margin,
+                           int scale)
 {
-    int width = barcode.size() + 2 * margin;
+    int logicalWidth = barcode.size() + 2 * margin;
+    int width = logicalWidth * scale;
 
     std::vector<unsigned char> image(width * height * 3);
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
 
+            int logicalX = x / scale;
             bool isBlack = false;
 
-            if (x >= margin && x < margin + barcode.size())
-                isBlack = barcode[x - margin] == 1;
+            if (logicalX >= margin && logicalX < margin + barcode.size()) {
+                isBlack = barcode[logicalX - margin] == 1;
+            }
 
             int index = (y * width + x) * 3;
 
-            if (isBlack) {
-                image[index + 0] = 0;
-                image[index + 1] = 0;
-                image[index + 2] = 0;
-            } else {
-                image[index + 0] = 255;
-                image[index + 1] = 255;
-                image[index + 2] = 255;
-            }
+            unsigned char color = isBlack ? 0 : 255;
+
+            image[index + 0] = color;
+            image[index + 1] = color;
+            image[index + 2] = color;
         }
     }
 
@@ -42,14 +41,17 @@ void BarcodeImage::savePNG(const std::string& filename,
                    image.data(), width * 3);
 }
 
-// ================= SVG =================
+// SVG
 
 void BarcodeImage::saveSVG(const std::string& filename,
                            const std::vector<int>& barcode,
                            int height,
-                           int margin)
+                           int margin,
+                           int scale)
 {
-    int width = barcode.size() + 2 * margin;
+    int logicalWidth = barcode.size() + 2 * margin;
+    int width = logicalWidth * scale;
+
 
     std::ofstream file(filename);
 
@@ -60,8 +62,9 @@ void BarcodeImage::saveSVG(const std::string& filename,
 
     for (int x = 0; x < barcode.size(); x++) {
         if (barcode[x] == 1) {
-            file << "<rect x='" << x + margin
-                 << "' y='0' width='1' height='" << height
+            file << "<rect x='" << (x + margin) * scale
+                 << "' y='0' width='" << scale
+                 << "' height='" << height
                  << "' fill='black'/>\n";
         }
     }
